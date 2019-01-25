a10e224a3ab61b4f9a26fbf915fbd66a2330989d
SimasDei <simondeiman@gmail.com>
2019-01-25 07:31:56 +0200

    folder restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { AccomodationComponent } from './accomodation/accomodation.component';
-import { RoomsComponent } from './accomodation/rooms/rooms.component';
-import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
-import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
-import { BarsComponent } from './food-beverage/bars/bars.component';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+import { RoomsComponent } from './accommodation/rooms/rooms.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { RestaurantComponent } from './food-and-beverage/restaurant/restaurant.component';
+import { BarsComponent } from './food-and-beverage/bars/bars.component';
 import { LifestyleComponent } from './lifestyle/lifestyle.component';
-import { WelnessAndSpaComponent } from './lifestyle/welness-and-spa/welness-and-spa.component';
+import { WellnessAndSpaComponent } from './lifestyle/wellness-and-spa/wellness-and-spa.component';
 import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
 import { ExperiencesComponent } from './lifestyle/experiences/experiences.component';
 import { ExcursionsComponent } from './lifestyle/excursions/excursions.component';
@@ -24,7 +24,27 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, AccomodationComponent, RoomsComponent, FoodBeverageComponent, RestaurantComponent, BarsComponent, LifestyleComponent, WelnessAndSpaComponent, ResortActivitiesComponent, ExperiencesComponent, ExcursionsComponent, WeddingsComponent, ThaiWeddingsComponent, WesternWeddingsComponent, CelebrationsComponent, OffersComponent, AuthComponent, SigninComponent, SignupComponent],
+  declarations: [
+    AppComponent,
+    AccommodationComponent,
+    RoomsComponent,
+    FoodAndBeverageComponent,
+    RestaurantComponent,
+    BarsComponent,
+    LifestyleComponent,
+    WellnessAndSpaComponent,
+    ResortActivitiesComponent,
+    ExperiencesComponent,
+    ExcursionsComponent,
+    WeddingsComponent,
+    ThaiWeddingsComponent,
+    WesternWeddingsComponent,
+    CelebrationsComponent,
+    OffersComponent,
+    AuthComponent,
+    SigninComponent,
+    SignupComponent
+  ],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
